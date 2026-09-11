cfd7b18426cad70549fd5ca8728a958310e61d56
Ankur <ankurku7365@gmail.com>
2026-09-11 12:35:57 +0530

    Created accountService

---
+++ src/main/java/com/BankingSystem/Banking_System/service/AccountService.java
@@ -0,0 +1,66 @@
+package com.BankingSystem.Banking_System.service;
+
+import com.BankingSystem.Banking_System.dto.AccountResponse;
+import com.BankingSystem.Banking_System.dto.CreateAccountRequest;
+import com.BankingSystem.Banking_System.entity.Account;
+import com.BankingSystem.Banking_System.entity.User;
+import com.BankingSystem.Banking_System.exception.UserNotFoundException;
+import com.BankingSystem.Banking_System.repository.AccountRepository;
+import com.BankingSystem.Banking_System.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+
+
+    public AccountService(AccountRepository accountRepository, UserRepository userRepository) {
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
+    }
+
+    public AccountResponse createAccount(CreateAccountRequest request){
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() ->
+                        new UserNotFoundException("User doesn't exist"));
+
+        String accountNumber = generateAccountNumber();
+
+        Account account = new Account();
+
+        account.setAccountNumber(accountNumber);
+        account.setAccountType(request.getAccountType());
+        account.setBalance(BigDecimal.ZERO);
+        account.setUser(user);
+
+
+        Account savedAccount = accountRepository.save(account);
+
+        AccountResponse accountResponse = new AccountResponse();
+
+        accountResponse.setId(savedAccount.getId());
+        accountResponse.setAccountNumber(savedAccount.getAccountNumber());
+        accountResponse.setAccountType(savedAccount.getAccountType());
+        accountResponse.setBalance(savedAccount.getBalance());
+        accountResponse.setCreatedAt(savedAccount.getCreatedAt());
+        accountResponse.setUserId(savedAccount.getUser().getId());
+
+        return accountResponse;
+
+
+
+    }
+
+    private String generateAccountNumber() {
+        Random random = new Random();
+
+        long number = 1000000000L + random.nextLong(900000000L);
+
+        return String.valueOf(number);
+    }
+}
