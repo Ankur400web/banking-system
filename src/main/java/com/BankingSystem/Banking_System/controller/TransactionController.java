829974ddfd28ceaf68088b2b2f52cea6c2e0dea4
Ankur <254418356+Ankur400web@users.noreply.github.com>
2026-09-13 09:59:37 +0530

    Created TransactionController and tested deposit method on postman that worked succcessfully

---
+++ src/main/java/com/BankingSystem/Banking_System/controller/TransactionController.java
@@ -0,0 +1,26 @@
+package com.BankingSystem.Banking_System.controller;
+
+import com.BankingSystem.Banking_System.dto.DepositRequest;
+import com.BankingSystem.Banking_System.dto.TransactionResponse;
+import com.BankingSystem.Banking_System.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/deposit")
+    public TransactionResponse deposit(@Valid @RequestBody DepositRequest depositRequest){
+        return transactionService.deposit(depositRequest);
+    }
+}
