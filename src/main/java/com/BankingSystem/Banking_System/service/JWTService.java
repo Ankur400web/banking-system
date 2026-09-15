fffe01c3b0ead5841a51ebccaf83eeba5f5cdd3e
Ankur <254418356+Ankur400web@users.noreply.github.com>
2026-09-15 19:55:14 +0530

    JWT under process

--- src/main/java/com/BankingSystem/Banking_System/service/JWTService.java
+++ src/main/java/com/BankingSystem/Banking_System/service/JWTService.java
@@ -1,15 +1,24 @@
 package com.BankingSystem.Banking_System.service;
 
 
+import io.jsonwebtoken.security.Keys;
 import lombok.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class JWTService {
+
 
     @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.expiration}")
     private long expiration;
+
+    SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+
 }
